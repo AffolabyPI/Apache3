5ad6accd6495b584fcefb0531c7a93792337cbee
kevin MESSIAEN <kermekx@gmail.com>
2016-02-03 15:26:14 +0100

    Apache 3 !

--- webserver/socket.c
+++ webserver/socket.c
@@ -1,4 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <unistd.h>
+#include <signal.h>
 
 int create_server(int port) {
-  return port;
+  int server_socket;
+
+  server_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+  if (server_socket == -1) {
+    perror("server_socket");
+    return -1;
+  }
+
+  struct sockaddr_in saddr;
+  saddr.sin_family = AF_INET;
+  saddr.sin_port = htons(port);
+  saddr.sin_addr.s_addr = INADDR_ANY;
+
+  if (bind(server_socket, (struct sockaddr *) &saddr, sizeof(saddr)) == -1) {
+    perror("bind server_socket");
+  }
+
+  if (listen(server_socket, 10) == -1) {
+    perror("lister server_socket");
+  }
+
+  int client_socket;
+  client_socket = accept(server_socket, NULL, NULL);
+
+  if(client_socket == -1) {
+    perror("accept");
+  }
+
+  const char *welcome_message = "Welcome on Apache 3!";
+  write(client_socket, welcome_message, strlen(welcome_message));
+  
+  return server_socket;
 }
--- webserver/socket.h
+++ webserver/socket.h
@@ -4,13 +4,3 @@
 int create_server(int port);
 
 #endif
-
-struct sockaddr_in { 
-sa_family_t sin_family; /* address family: AF_INET */ 
-in_port_t sin_port; /* port in network byte order */ 
-struct in_addr sin_addr; /* internet address */ 
-};
-
-struct in_addr { 
-uint32_t s_addr; /* address in network byte order */ 
-}; 
