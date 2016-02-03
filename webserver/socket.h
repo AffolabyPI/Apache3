af95b9d3e1fb57d9e4ade14c0770306fa4a69e1e
Affolaby Paul-Ivan <paulivanaffolabyn@hotmail.com>
2016-02-03 15:23:36 +0100

    Socket.h fonctionnel

--- webserver/socket.h
+++ webserver/socket.h
@@ -4,15 +4,3 @@
 int create_server(int port);
 
 #endif
-<<<<<<< HEAD
-=======
-
-struct sockaddr_in saddr;
-saddr.sin_family = AF_INET ; /* Socket ipv4 */
-saddr.sin_port = htons(8080); /* Port d ’ écoute */
-saddr.sin_addr.s_addr = INADDR_ANY ; /* écoute sur toutes les interfaces */
-if(bind(socket_serveur, (struct sockaddr *)&saddr, sizeof(saddr)) == -1) {
-perror ("bind socker_serveur");
-/* traitement de l ’ erreur */
-}
->>>>>>> refs/remotes/origin/master
