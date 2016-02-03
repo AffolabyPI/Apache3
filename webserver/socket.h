03300a67380265bbb656df68886031ca64753e00
flotan11 <mardon.florian@gmail.com>
2016-02-03 15:03:17 +0100

    Add struct sockaddr to socket.h

--- webserver/socket.h
+++ webserver/socket.h
@@ -4,3 +4,13 @@
 int create_server(int port);
 
 #endif
+
+struct sockaddr_in { 
+sa_family_t sin_family; /* address family: AF_INET */ 
+in_port_t sin_port; /* port in network byte order */ 
+struct in_addr sin_addr; /* internet address */ 
+};
+
+struct in_addr { 
+uint32_t s_addr; /* address in network byte order */ 
+}; 
