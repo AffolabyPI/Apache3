663e50f32f9692906f8be51ef9966d24ee56cb0d
kevin MESSIAEN <kermekx@gmail.com>
2016-02-03 17:27:47 +0100

    echo

--- webserver/socket.c
+++ webserver/socket.c
@@ -74,6 +74,18 @@ int accept_client(int server_socket) {
 
   if(fork() == 0){
     write(client_socket, welcome_message, strlen(welcome_message));
+    
+    char* buffer[1024];
+    int readed = 0;
+    
+    while(1) {
+      if((readed = read(client_socket, buffer, sizeof(buffer))) == -1) {
+        perror("read client");
+	return -1;
+      }
+      write(client_socket, buffer, readed);
+    }
+    
     close(server_socket);
     close(client_socket);
     exit(1);
