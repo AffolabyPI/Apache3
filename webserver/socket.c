c2976e964118abca4513b26338290efa6f65b665
kevin MESSIAEN <kermekx@gmail.com>
2016-03-09 14:18:56 +0100

    parse_http_request

--- webserver/socket.c
+++ webserver/socket.c
@@ -9,6 +9,7 @@
 #include <unistd.h>
 #include <signal.h>
 #include <errno.h>
+#include "socket.h"
 
 const char *welcome_message = "Welcome on Apache 3 !!! Le serveur du futur !\n Il a ete entierement realiser suite a des \netudes francaise que j'ai realise moi-meme car je sui \nfrancais, prenant en compte l'influence d'apache dans le \nmonde des devellopeurs de l'IUT de Lille A au seins de la \npromo Promo N4P2. Ce serveur est concus par les meilleurs \netudiants de la promotion, a savoir : Paul-Ivan Affolaby, \nexpert monetaire doue d'un esprit de chef d'equipe, Kevin \nMessien, technicien de pointe capable de realiser des chmod \nincongru (notemment pour se retirer ses propres droits de \nfacon recursif), son genie reste inegale, et enfin Florian \nMardon, futur ingenieur expert en lardon et en pate-raclette";
 const char *apache3 = "<Apache3>";
@@ -74,43 +75,54 @@ void fgets_or_exit(char *buffer, int size, FILE *stream){
     perror("Cannot read");
     exit(1);
   }
-  printf("%s", buffer);
+}
+
+int parse_http_request(char *request_line, http_request *request){
+  char* token = strtok(request_line, " ");
+  int line = 0;
+  while(token != NULL) {
+    if (line == 0 && strcmp("GET", token) == 0) {
+      request->method = HTTP_GET;
+    } else if (line == 0) {
+      request->method = HTTP_UNSUPPORTED;
+    } else if (line == 1) {
+      request->url = token;
+    } else if (line == 2) {
+      if (strcmp(" HTTP/1.0\r\n", token) == 0) {
+	request->major_version = 1;
+	request->minor_version = 0;
+      } else if (strcmp(" HTTP/1.1\r\n", token) != 0) {
+	request->major_version = 1;
+	request->minor_version = 1;
+      } else {
+	return 0;
+      }
+    } else {
+      return 0;
+    }
+    line++;
+    token = strtok(NULL, " ");
+  }
+  return line == 3;
 }
 
 int check_client_header(FILE *file) {
   char buffer[1024];
+  http_request request;
 
   fgets_or_exit(buffer, 1024, file);
 
-  char *c = buffer;
-  char *words[2];
-
-  if (strlen(c) < 3 && (c[0] != 'G' || c[1] != 'E' || c[2] != 'T')) {
+  if (parse_http_request(buffer, &request) == 1) {
+    if (request.method != HTTP_GET) {
+      return 400;
+    } else if (strcmp(request.url, "/")) {
+      return 404;
+    } else {
+      return 200;
+    }
+  } else {
     return 400;
   }
-
-  int word = 0;
-
-  while(c[0] != '\0') {
-   if(c[0] == ' ') {
-     if (word == 2) {
-       return 400;
-     }
-     words[word] = c;
-     word ++;
-   }
-   c++;
- }
-
- if (strcmp(" HTTP/1.0\r\n", words[1]) != 0 && strcmp(" HTTP/1.1\r\n", words[1]) != 0) {
-  return 400;
-}
-
-if (words[0][1] != '/' || words[0][2] != ' ') {
- return 404;
-}
-
-return 200;
 }
 
 int accept_client(int server_socket) {
@@ -128,9 +140,7 @@ int accept_client(int server_socket) {
     int headerError = 0;
     headerError = check_client_header(file);
 
-
-    char buffer[1024];
-
+    char buffer[1024];		       
     int fin = 0;
 
     while (fin == 0) {
@@ -146,7 +156,7 @@ int accept_client(int server_socket) {
       fprintf(file, "%s", error_404);
     } else if (headerError == 200) {
       fprintf(file, "%s%s%d\r\n\r\n%s %s", message_200, message_size, (int) (strlen(apache3) + strlen(welcome_message) + 3), apache3, welcome_message);
-    } 
+    }
 
     fflush(file);   
     close(client_socket);
@@ -156,38 +166,6 @@ int accept_client(int server_socket) {
   close(client_socket);
 
   return 0;
-}
-
-int parse_http_request(const char *request_line, http_request *request){
-  int ok;
-  token = strtok(request_line, " ");
-  while(token){
-      ++tkCounter;
-      if(tkCounter==1 && strcmp(token,"GET")!=0){
-        ok = 0;
-      }
-      if(tkCounter==2 && strcmp(token, "/")!=0){
-        ok = 1;
-      }       
-      if(tkCounter > 3){
-        ok = 0;
-      }
-      if(tkCounter == 3 && strcmp(token,"HTTP/1.0\r\n")!=0 && strcmp(token,"HTTP/1.1\r\n")!=0){
-        ok = 0;
-      }
-      token=strtok(NULL," ");
-  } 
-  if(tkCounter < 3 ){
-    ok = 0;
-  }
-  tkCounter=0;
-  while(ok==1 && ligneVide==0){
-    str = fgets_or_exit(buffer, 80, file);
-    if(str!= NULL && (strcmp(str, "\r\n")==0 || strcmp(str, "\n")==0)){
-      ligneVide=1;
-    }
-  }
-  return ok;
 }
 
 void close_client(int client_socket) {
